a2670dbc23eb52eb88c9c7ee8888b3ead5375af0
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-21 15:27:15 -0500

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let s = "";
+    if (num >= 0){
+        for (let i = 0; i < num; i++){
+            s += string;
+        }
+        return s;
+    }
+    else{
+        return "ERROR";
+    }    
 };
 
 // Do not edit below this line
