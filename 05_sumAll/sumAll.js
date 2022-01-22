92cd7ce74816139dc7202380266a17cd948fec9a
John Lagoutaris <jjlagoutaris@gmail.com>
2022-01-22 11:08:26 -0500

    skipped over 4 for now, finished 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (typeof(start) === "number" && typeof(end) == "number"){
+        if (start > end){
+            placeholder = start;
+            start = end;
+            end = placeholder;
+        }
+        if (start < 0 || end < 0){
+            return "ERROR";
+        }
+        for(let i = start; i <= end; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else{
+        return "ERROR";
+    }
+        
 };
 
 // Do not edit below this line
